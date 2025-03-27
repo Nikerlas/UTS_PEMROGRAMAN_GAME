054ee6a807d0ac6712df2a33520c9ae148fed2ce
NicholasErnesto <nicholas.ernesto23@gmail.com>
2025-03-28 01:21:46 +0700

    UI LEVEL

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -6,33 +6,21 @@ using UnityEngine.UIElements;
 
 public class SceneManagement : MonoBehaviour
 {
-    public string EnterScene;
-    public string ExitScene;
-    public bool isEscapeToExit = false;
-    // Start is called before the first frame update
-    void Start()
+    public void PilihPermainan()
     {
-        
+        SceneManager.LoadScene("Level");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Escape))
-        {
-            if(isEscapeToExit)
-            {
-                Application.Quit();
-            }
-            else
-            {
-                BackToMenu();
-            }
-        }
-    }
-
-    public void BackToMenu()
+    public void KembaliKeMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+    public void CreditScene()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
     }
 }
