53db6cfcca875f3e70234c982a7e8ef3b80a1271
NicholasErnesto <nicholas.ernesto23@gmail.com>
2025-04-21 23:01:38 +0700

    garap game 2

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,31 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
-    public float batasAtas;
-    public float batasBawah;
-    public float kecepatan;
-    public string axis;
-    public string axisX;
+    public float batasAtas = 4.5f;
+    public float batasBawah = -4.5f;
+    public float batasKiri = -7f;
+    public float batasKanan = 7f;
+
+    public float kecepatan = 5f;
+    public string axis = "Vertical";
+    public string axisX = "Horizontal";
 
     void Update()
     {
-        float gerak = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+        float gerakY = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
         float gerakX = Input.GetAxis(axisX) * kecepatan * Time.deltaTime;
 
-        float nextPos = transform.position.y + gerak;
-        float nextPosX = transform.position.x + gerakX;
+        Vector3 nextPos = transform.position + new Vector3(gerakX, gerakY, 0);
 
-        if(nextPos > batasAtas)
-        {
-            gerak = 0;
-        }
-        if(nextPos < batasBawah)
-        {
-            gerak = 0;
-        }
-        transform.Translate(gerakX, gerak, 0);
+        // Clamp biar tidak menembus tepian
+        nextPos.y = Mathf.Clamp(nextPos.y, batasBawah, batasAtas);
+        nextPos.x = Mathf.Clamp(nextPos.x, batasKiri, batasKanan);
+
+        transform.position = nextPos;
     }
 }
