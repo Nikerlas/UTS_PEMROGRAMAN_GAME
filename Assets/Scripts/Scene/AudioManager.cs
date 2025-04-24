0d767e3e24dfb917a37c3f7c9cd009fe2a809f86
NicholasErnesto <nicholas.ernesto23@gmail.com>
2025-04-24 07:05:19 +0700

    menu

---
+++ Assets/Scripts/Scene/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip menuAndLevelMusic;
+    private AudioClip otherSceneMusic;
+
+    private AudioSource audioSource;
+    private static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string sceneName = scene.name;
+
+        if (sceneName == "Menu" || sceneName == "Level" || sceneName == "Credit")
+        {
+            if (audioSource.clip != menuAndLevelMusic)
+            {
+                audioSource.clip = menuAndLevelMusic;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.clip != otherSceneMusic)
+            {
+                audioSource.clip = otherSceneMusic;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+        }
+    }
+}
