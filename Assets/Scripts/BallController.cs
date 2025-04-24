b049af86f901599c861b3e7f9163d35e2c6c11c7
NicholasErnesto <nicholas.ernesto23@gmail.com>
2025-04-24 18:54:29 +0700

    game 2

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,110 +4,112 @@ using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
-    public int force = 300;
-    public AudioClip hitSound;
-    public AudioClip goalWhistle;
-    public GoalPanelAnimator goalPanelAnimator;
-    int scoreP1 = 0;
-    int scoreP2 = 0;
+    int scoreP1;
+    int scoreP2;
 
     Text scoreUIP1;
     Text scoreUIP2;
 
-    AudioSource audioSource;
+    AudioSource audioPlay;
+    public AudioClip hitSound;
+    public AudioClip peluitClip;
+
     GameObject panelSelesai;
     Text txtPemenang;
 
+    GameObject panelGoal;
+
+    public int force;
     Rigidbody2D rb;
-    GameObject paddle1, paddle2;
-    Vector2 initialBallPos = Vector2.zero;
-    Vector2 initialPaddle1Pos;
-    Vector2 initialPaddle2Pos;
+
+    Vector2 startPos;
+    Vector2 paddle1StartPos;
+    Vector2 paddle2StartPos;
+
+    GameObject paddle1;
+    GameObject paddle2;
 
     void Start()
     {
+        scoreP1 = 0;
+        scoreP2 = 0;
+
         rb = GetComponent<Rigidbody2D>();
-        audioSource = GetComponent<AudioSource>();
+        audioPlay = GetComponent<AudioSource>();
 
         scoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
         scoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+
         panelSelesai = GameObject.Find("PanelSelesai");
-        txtPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
         panelSelesai.SetActive(false);
+
+        panelGoal = GameObject.Find("PanelGoal");
+        panelGoal.SetActive(false);
+
+        startPos = transform.localPosition;
 
         paddle1 = GameObject.Find("Paddle1");
         paddle2 = GameObject.Find("Paddle2");
+        paddle1StartPos = paddle1.transform.localPosition;
+        paddle2StartPos = paddle2.transform.localPosition;
 
-        initialPaddle1Pos = paddle1.transform.position;
-        initialPaddle2Pos = paddle2.transform.position;
-
-        StartCoroutine(StartGame());
+        StartCoroutine(JedaDanReset(1));
     }
 
-    IEnumerator StartGame()
+    private void OnCollisionEnter2D(Collision2D coll)
     {
-        yield return new WaitForSeconds(0.5f);
-        Vector2 arah = new Vector2(Random.Range(0, 2) == 0 ? -1 : 1, 0).normalized;
-        rb.AddForce(arah * force);
-    }
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.name == "Kanan")
+        if (coll.gameObject.name == "GawangKanan")
         {
-            scoreP1++;
+            scoreP1 += 1;
             TampilkanScore();
 
             if (scoreP1 == 5)
             {
                 panelSelesai.SetActive(true);
+                txtPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
                 txtPemenang.text = "Player 1 Menang!";
                 Destroy(gameObject);
+                return;
             }
-            else
-            {
-                StartCoroutine(HandleGoal(true));
-            }
+
+            StartCoroutine(TampilkanGoalDanReset("Player 1", 1));
         }
-        else if (coll.gameObject.name == "Kiri")
+        else if (coll.gameObject.name == "GawangKiri")
         {
-            scoreP2++;
+            scoreP2 += 1;
             TampilkanScore();
 
             if (scoreP2 == 5)
             {
                 panelSelesai.SetActive(true);
+                txtPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
                 txtPemenang.text = "Player 2 Menang!";
                 Destroy(gameObject);
+                return;
             }
-            else
-            {
-                StartCoroutine(HandleGoal(false));
-            }
+
+            StartCoroutine(TampilkanGoalDanReset("Player 2", -1));
         }
         else if (coll.gameObject.name == "Paddle1" || coll.gameObject.name == "Paddle2")
         {
-            audioSource.PlayOneShot(hitSound);
+            audioPlay.PlayOneShot(hitSound);
+
             float sudut = (transform.position.y - coll.transform.position.y) * 5f;
-            Vector2 arah = new Vector2(rb.velocity.x, sudut).normalized;
+            Vector2 arah = new Vector2(rb.velocity.x, sudut);
+
+            if (Mathf.Abs(arah.y) < 0.5f)
+            {
+                arah.y = 0.5f * Mathf.Sign(arah.y != 0 ? arah.y : Random.Range(-1f, 1f));
+            }
+
+            arah = arah.normalized;
             rb.velocity = Vector2.zero;
             rb.AddForce(arah * force * 2);
         }
-    }
-
-    IEnumerator HandleGoal(bool leftToRight)
-    {
-        rb.velocity = Vector2.zero;
-        transform.position = initialBallPos;
-        paddle1.transform.position = initialPaddle1Pos;
-        paddle2.transform.position = initialPaddle2Pos;
-
-        goalPanelAnimator.PlayGoalAnimation(leftToRight);
-        audioSource.PlayOneShot(goalWhistle);
-
-        yield return new WaitForSeconds(2f);
-        Vector2 arah = new Vector2(leftToRight ? -1 : 1, 0).normalized;
-        rb.AddForce(arah * force);
+        else
+        {
+            audioPlay.PlayOneShot(hitSound);
+        }
     }
 
     void TampilkanScore()
@@ -116,4 +118,41 @@ public class BallController : MonoBehaviour
         scoreUIP2.text = scoreP2.ToString();
     }
 
+    IEnumerator TampilkanGoalDanReset(string player, int arahX)
+    {
+        panelGoal.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        panelGoal.SetActive(false);
+
+        StartCoroutine(JedaDanReset(arahX));
+    }
+
+    IEnumerator JedaDanReset(int arahX)
+    {
+        rb.velocity = Vector2.zero;
+        transform.localPosition = startPos;
+
+        paddle1.transform.localPosition = paddle1StartPos;
+        paddle2.transform.localPosition = paddle2StartPos;
+
+        // Nonaktifkan kontrol paddle (ganti PaddleController dengan script kamu)
+        paddle1.GetComponent<PaddleController>().enabled = false;
+        paddle2.GetComponent<PaddleController>().enabled = false;
+
+        yield return new WaitForSeconds(1f);
+        audioPlay.PlayOneShot(peluitClip);
+
+        // Aktifkan kembali kontrol paddle
+        paddle1.GetComponent<PaddleController>().enabled = true;
+        paddle2.GetComponent<PaddleController>().enabled = true;
+
+        MulaiPermainan(arahX);
+    }
+
+    void MulaiPermainan(int arahX)
+    {
+        Vector2 arah = new Vector2(arahX, 0).normalized;
+        rb.AddForce(arah * force);
+    }
 }
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -11,17 +11,50 @@ public class PaddleController : MonoBehaviour
     public string axis = "Vertical";
     public string axisX = "Horizontal";
 
+    public bool isAI = false;         // Aktifkan jika paddle ini dikendalikan AI
+    public Transform bola;           // Drag objek bola ke sini dari inspector
+    public float kecepatanAI = 4f;   // AI bisa beda kecepatannya
+
     void Update()
+    {
+        if (isAI)
+        {
+            JalankanAI();
+        }
+        else
+        {
+            JalankanManual();
+        }
+    }
+
+    void JalankanManual()
     {
         float gerakY = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
         float gerakX = Input.GetAxis(axisX) * kecepatan * Time.deltaTime;
 
         Vector3 nextPos = transform.position + new Vector3(gerakX, gerakY, 0);
 
-        // Clamp biar tidak menembus tepian
         nextPos.y = Mathf.Clamp(nextPos.y, batasBawah, batasAtas);
         nextPos.x = Mathf.Clamp(nextPos.x, batasKiri, batasKanan);
 
         transform.position = nextPos;
+    }
+
+    void JalankanAI()
+    {
+        if (bola == null) return;
+
+        Vector3 targetPos = transform.position;
+
+        // Gerakkan paddle hanya di sumbu Y (bisa diubah kalau mau gerakan X juga)
+        if (bola.position.y > transform.position.y + 0.1f)
+            targetPos.y += kecepatanAI * Time.deltaTime;
+        else if (bola.position.y < transform.position.y - 0.1f)
+            targetPos.y -= kecepatanAI * Time.deltaTime;
+
+        targetPos.y = Mathf.Clamp(targetPos.y, batasBawah, batasAtas);
+        targetPos.x = Mathf.Clamp(targetPos.x, batasKiri, batasKanan);
+
+        transform.position = targetPos;
     }
 }
