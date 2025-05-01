a16996bd411b28febe034c047ded078f2886333f
AhmadRizkiadi <rizkiahmad7396@gmail.com>
2025-05-01 19:11:23 +0700

    game3-init

---
+++ Assets/Scripts/Game3/MunculHewan.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculHewan : MonoBehaviour
+{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] obyekHewan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= jeda)
+        {
+            int random = Random.Range(0, obyekHewan.Length);
+            Instantiate(obyekHewan[random], transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
